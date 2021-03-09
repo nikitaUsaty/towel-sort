2cb6a74ea6591a89db9a0d671964b3e8d48f53be
nikitausaty <nikitausatiy@yandex.ru>
2021-03-10 00:49:34 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix) return [];
+    for (let i = 1; i < matrix.length; i += 2) {
+        matrix[i].reverse();
+    }
+    return [].concat.apply([], matrix);
+};
